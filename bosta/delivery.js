50ed368fa68edfadee61dc023be65c8ac6f236ad
ElGendy <ahmedelgendy409@gmail.com>
2020-09-10 18:34:57 +0200

    delivery and pickup wrapper

--- bosta/delivery.js
+++ bosta/delivery.js
@@ -1,27 +1,105 @@
-const createDelivery = () => {
+const axios = require('axios');
+const config = require('./config.json');
+const API_KEY = config.BOSTA_API_KEY;
+const BASE_URL = config.BOSTA_API_BASE;
 
-};
+class Delivery {
 
-const getBusinessDeliveries = () => {
+    async createDelivery(
+        type,
+        specs,
+        notes,
+        cod,
+        dropOffAddress,
+        businessReference,
+        receiver,
+    ) {
+        const data = {
+            type,
+            specs,
+            notes,
+            cod,
+            dropOffAddress,
+            businessReference,
+            receiver,
+        };
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-};
+        return axios({
+            method: 'POST',
+            url: `${BASE_URL}/deliveries/`,
+            headers,
+            data,
+        });
+    }
 
-const getDeliveriesSummary = () => {
+    async getAllDeliveries() {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-};
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/deliveries/`,
+            headers,
+        });
+    }
 
-const getAirwayBills = () => {
+    async getDeliveryByTrakingNumber(trackingNumber) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-};
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/deliveries/${trackingNumber}`,
+            headers,
+        });
+    }
 
-const getDeliveyByTrackingNumber = () => {
+    async trackDelivery(trackingNumber) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-};
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/deliveries/${trackingNumber}/tracking`,
+            headers,
+        });
+    }
 
-module.exports = {
-    createDelivery,
-    getBusinessDeliveries,
-    getDeliveriesSummary,
-    getAirwayBills,
-    getDeliveyByTrackingNumber,
-};
+    async printDeliveryAWB(deliveryId) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
+
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/deliveries/awb/${deliveryId}`,
+            headers,
+        });
+    }
+
+    async terminateDelivery(deliveryId) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
+
+        return axios({
+            method: 'DELETE',
+            url: `${BASE_URL}/deliveries/${deliveryId}`,
+            headers,
+        });
+    }
+}
+
+export default new Delivery();
--- bosta/pickupRequest.js
+++ bosta/pickupRequest.js
@@ -1,17 +1,89 @@
-const createNewPickupRequest = () => {
+const axios = require('axios');
+const config = require('./config.json');
+const API_KEY = config.BOSTA_API_KEY;
+const BASE_URL = config.BOSTA_API_BASE;
 
-};
+class Pickup {
+    
+    async getBusinessPickupLocations() {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-const getBusinessPickupRequests = () => {
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/pickup-locations/`,
+            headers,
+            data,
+        });
+    }
 
-};
+    async createPickup(
+        businessLocationId,
+        notes,
+        scheduledDate,
+        scheduledTimeSlot,
+        contactPerson,
+    ) {
+        const data = {
+            businessLocationId,
+            notes,
+            scheduledDate,
+            scheduledTimeSlot,
+            contactPerson,
+        };
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-const getPickupRequestById = () => {
+        return axios({
+            method: 'POST',
+            url: `${BASE_URL}/pickups/`,
+            headers,
+            data,
+        });
+    }
 
-};
+    async getAllPickups() {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
 
-module.exports = {
-    createNewPickupRequest,
-    getBusinessPickupRequests,
-    getPickupRequestById,
-};
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/pickups/`,
+            headers,
+        });
+    }
+
+    async getPickupById(pickupId) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
+
+        return axios({
+            method: 'GET',
+            url: `${BASE_URL}/pickups/${pickupId}`,
+            headers,
+        });
+    }
+
+    async deletePickup(pickupId) {
+        const headers = {
+            Authorization: API_KEY,
+            contentType: 'application/json',
+        };
+
+        return axios({
+            method: 'DELETE',
+            url: `${BASE_URL}/pickups/${pickupId}`,
+            headers,
+        });
+    }
+}
+
+export default new Pickup();
